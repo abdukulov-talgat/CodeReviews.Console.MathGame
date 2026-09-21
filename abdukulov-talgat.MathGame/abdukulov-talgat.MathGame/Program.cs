a45bae5e5ff3a88d4ddc5142a3fda0096bbbbefa
abdukulov-talgat <abdukulov.talgat@gmail.com>
2026-09-21 12:22:21 +0500

    Correct EntryPoint

--- abdukulov-talgat.MathGame/abdukulov-talgat.MathGame/Program.cs
+++ abdukulov-talgat.MathGame/abdukulov-talgat.MathGame/Program.cs
@@ -1,9 +1,25 @@
-﻿namespace abdukulov_talgat.MathGame;
+﻿using abdukulov_talgat.MathGame.Helpers;
+using abdukulov_talgat.MathGame.UI;
+
+namespace abdukulov_talgat.MathGame;
 
 internal static class Program
 {
-    private static void Main(string[] args)
+    private const int RoundsPerSession = 5;
+
+    private static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine(string.PadCenter());
+        Console.WriteLine(string.PadCenter("Math Game"));
+        Console.WriteLine(string.PadCenter());
+        Console.WriteLine("\n");
+
+        MainMenuState mainMenuState = new();
+        GameFlowContext context = new(RoundsPerSession, mainMenuState);
+
+        context.StartGameLoop();
+
+        Console.WriteLine(string.PadCenter("Goodbye..."));
+        Thread.Sleep(TimeSpan.FromSeconds(1));
     }
 }
